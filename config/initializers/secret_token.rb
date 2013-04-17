a9db4673a14acf371365ff4bd7b7ee9c746d512e
Daniel Langer <danlan@seas.upenn.edu>
2013-04-16 21:16:49 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickByte::Application.config.secret_token = '1887b957c770db644ae4364f327841dec827bee5df4ce60ae56f1f8921ae4970c4958c01d910b59527ec5747b37c33a9bea2f2b370d33018fb25051c70dd8628'
