ef357bb110b04b7f7122ce0251655ff6cee014cf
Daniel Langer <danlan@seas.upenn.edu>
2013-04-22 22:24:22 -0400

    rake changes

--- db/migrate/20130423012626_add_alert_location_to_users.rb
+++ db/migrate/20130423012626_add_alert_location_to_users.rb
@@ -1,6 +1,5 @@
 class AddAlertLocationToUsers < ActiveRecord::Migration
   def change
     add_column :users, :alert_location, :integer
-    remove_column :group_meals, :alert_location, :integer
   end
 end
